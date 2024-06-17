bcde89573a24b7ea59851a5647d5e5d07b12aa74
Matthias Winzeler <matthias.winzeler@gmail.com>
2024-06-17 17:30:56 +0200

    add example

---
+++ examples/provider-install-verification/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    melt = {
+      source = "meltcloud.io/melt/melt"
+    }
+  }
+}
+
+provider "melt" {
+  endpoint = "http://localhost:3000"
+}
+
+resource "melt_cluster" "example" {
+  name    = "melt02"
+  version = "1.28"
+}
+
+resource "melt_machine_pool" "example" {
+  cluster_id = melt_cluster.example.id
+
+  name                = "pool2"
+  version             = "1.28"
+  primary_disk_device = "/dev/vda"
+}
+
+resource "melt_machine" "example" {
+  machine_pool_id = melt_machine_pool.example.id
+
+  uuid = "2005cc24-522a-4485-9b9a-e60a61d9f9cf"
+  name = "melt-node01"
+}
+
