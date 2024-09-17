c09a2809d301fa2397d397d4069312852db3fc6c
Matthias Winzeler <matthias.winzeler@gmail.com>
2024-09-17 17:20:41 +0200

    fix linting

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -5,15 +5,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
-const (
-	providerConfig = `
-provider "meltcloud" {
-  endpoint     = "https://app.meltcloud.io"
-  organization = "deadbeef-0000-0000-0000-000000000000"
-  api_key      = "dummy"
-}
-`
-)
+// currently unused
+//const (
+//	providerConfig = `
+//provider "meltcloud" {
+//  endpoint     = "https://app.meltcloud.io"
+//  organization = "deadbeef-0000-0000-0000-000000000000"
+//  api_key      = "dummy"
+//}
+//`
+//)
 
 var (
 	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
